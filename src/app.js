dfc7800a516643bbd04944a4607448561887d69e
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-05-17 17:00:50 +0300

    add carousel

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import ScrollReveal from 'scrollreveal';
+import Siema from 'siema';
 
+// scroll
 window.sr = ScrollReveal({
     reset: false,
     easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
 
-
-// screenshots
 const screenshotConfig = { delay: 200, duration: 700 };
 
 sr.reveal('.js-screenshot-first', screenshotConfig);
@@ -21,3 +21,23 @@ sr.reveal('.js-landing-section', {
     opaicty: 1,
     viewFactor: 0.00001
 });
+
+
+// siema
+const siema = new Siema({
+    selector: '.js-slider',
+    duration: 500,
+    easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+    perPage: 1,
+    startIndex: 0,
+    threshold: 20,
+    draggable: false,
+    loop: false
+});
+
+const dots = document.querySelectorAll('.js-dot');
+const dotsArray = [].map.call(dots, obj => obj);
+
+dotsArray.map((dot, i) => {
+    dot.addEventListener('click', () => siema.goTo(i))
+})
